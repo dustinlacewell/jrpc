54082ed9155da4daf5daf2dd8a847fca3ea0cb57
dlacewell <dlacewell@gmail.com>
2016-06-22 18:52:55 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -2,22 +2,18 @@
 
 from setuptools import setup
 
-setup(
-    name = "jrpc",
-    version = "0.1.1",
-    packages = ['jrpc'],
-    scripts = ['bin/jrpc'],
+setup(name="jrpc",
+      version="0.1.2",
+      packages=['jrpc'],
+      scripts=['bin/jrpc'],
+      install_requires=[
+          "Twisted",
+          "autobahn",
+      ],
 
-    install_requires = [
-        "Twisted",
-        "autobahn",
-    ],
-
-    # metadata for upload to PyPI
-    author = "Dustin Lacewell",
-    author_email = "dlacewell@gmail.com",
-    description = "JSON RPC for Autobahn/Twisted",
-    keywords = "twisted autobahn rpc json",
-    url = "http://github.com/dustinlacewell/jrpc",
-)
-
+      # metadata for upload to PyPI
+      author="Dustin Lacewell",
+      author_email="dlacewell@gmail.com",
+      description="JSON RPC for Autobahn/Twisted",
+      keywords="twisted autobahn rpc json",
+      url="http://github.com/dustinlacewell/jrpc", )
